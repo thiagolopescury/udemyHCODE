1aa94a5f4b218307d0b804bd98c2c65712466b8f
Thiago <thiagolopes0@gmail.com>
2019-11-08 13:13:01 -0300

    constant

---
+++ constante/exemplo1.php
@@ -0,0 +1,7 @@
+<?php
+
+define("SERVIDOR","192.168.0.1");
+
+echo SERVIDOR;
+
+?>
---
+++ constante/exemplo2.php
@@ -0,0 +1,10 @@
+<?php 
+
+define("BANCO_DE_DADOS",[
+    "192.168.0.1",
+    "root",
+    "password",
+    "test"
+]);
+print_r(BANCO_DE_DADOS);
+?>
