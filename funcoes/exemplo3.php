fd8e6a84cad204ce53f1beb164ced4b77044397f
Thiago <thiagolopes0@gmail.com>
2019-11-11 14:36:17 -0300

    funcoes

--- funcoes/exemplo3.php
+++ funcoes/exemplo3.php
@@ -1,10 +1,10 @@
 <?php
- 
+ // valores de parametros que não contem um valor padrão, SEMPRE  colocar primeiro 
  function Oi($letra, $periodo = "Bom dia"){
      return " Ola  $letra $periodo<br>";
  }
 
  echo oi("Thiago");
- echo oi("Lopes");
- echo oi("Cury");
+ echo oi("Lopes ", "Boa noite");
+ echo oi("Cury", "Boa tarde");
  ?>
---
+++ funcoes/testefuncoes.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+function somar($numero1,$numero2){
+
+    return $numero1 + $numero2;
+}
+
+echo somar(100,10);
+
+echo "<br>";
+
+function subtrair($numero1,$numero2){
+
+    return $numero1 - $numero2;
+}
+
+echo subtrair(100,10);
+
+echo "<br>";
+
+function multiplicar($numero1,$numero2){
+
+    return $numero1 * $numero2;
+}
+
+echo multiplicar(100,10);
+
+echo "<br>";
+
+function dividir($numero1,$numero2){
+
+    return $numero1 / $numero2;
+
+}
+echo dividir(100,10);
+
+
+
+?>
