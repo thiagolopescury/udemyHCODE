84d12e2fdde86cad3b79dd02c0b04d230b0ce3ae
Thiago <thiagolopes0@gmail.com>
2019-11-20 14:52:13 -0300

    funcoes

--- funcoes/exemplo8.php
+++ funcoes/exemplo8.php
@@ -1,13 +1,13 @@
 <?php
 
-function soma(int ...$valores):string {
+function soma(int ...$valores): string
+{
     return array_sum($valores);
 }
 
-echo soma(10,10,10,10,10);
-echo"<br>";
-echo soma(20,10);
-echo"<br>";
-echo soma(30,10);
-echo"<br>";
-?>
+echo soma(10, 10, 10, 10, 10);
+echo "<br>";
+echo soma(20, 10);
+echo "<br>";
+echo soma(30, 10);
+echo "<br>";
