4f4ab2ef25254c810abdfb7d0cfab7cc918c43a5
Thiago <thiagolopes0@gmail.com>
2019-12-12 16:01:40 -0300

    pdo

---
+++ pdo/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = new PDO("mysql:dbname=bdphp7;host=localhost", "root", "");
+
+$stmt = $conn->prepare("SELECT * FROM tbl_usuarios");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row) {
+    foreach ($row as $key => $value) {
+        echo "<strong>".$key.":</strong>".$value."<br>";
+    }
+
+    echo "====================================<br>";
+}
+
+?>
