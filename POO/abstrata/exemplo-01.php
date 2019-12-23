c69d7ccc91d47b85264d1fed588e6c9c33144c67
Thiago <thiagolopes0@gmail.com>
2019-12-23 14:38:41 -0300

    indentação

--- POO/abstrata/exemplo-01.php
+++ POO/abstrata/exemplo-01.php
@@ -1,39 +1,37 @@
-<?php 
+<?php
 
-interface Veiculo {
+interface Veiculo
+{
     public function acelerar($velocidade);
     public function frenar($frenar);
     public function trocarMarcha($trocarMarcha);
-    
 }
 
- abstract class Automovel implements Veiculo{
+abstract class Automovel implements Veiculo
+{
     public function acelerar($velocidade)
     {
-        echo " o veiculo acelerou ate" . $velocidade ;
+        echo " o veiculo acelerou ate" . $velocidade;
     }
 
     public function frenar($frenar)
     {
-        echo " o veiculo frenou ate " . $frenar ;
-
-
+        echo " o veiculo frenou ate " . $frenar;
     }
 
     public function trocarMarcha($trocarMarcha)
     {
-        echo " o veiculoengatou a marcha " . $trocarMarcha ;
+        echo " o veiculoengatou a marcha " . $trocarMarcha;
     }
 }
 
-class DelRey extends Automovel{
-    public function empurrar(){
-
+class DelRey extends Automovel
+{
+    public function empurrar()
+    {
     }
 }
 
 $carro = new DelRey();
 
-$carro ->acelerar(200);
- 
-?>
+$carro->acelerar(200);
