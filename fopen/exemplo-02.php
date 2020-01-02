37041f4cb23546ed699752229a6d4ba6fb190cad
Thiago <thiagolopes0@gmail.com>
2020-01-02 16:49:17 -0300

    link

--- fopen/exemplo-02.php
+++ fopen/exemplo-02.php
@@ -6,6 +6,26 @@ $sql = new Sql();
 
 $usuarios = $sql ->select("SELECT * FROM tbl_usuarios ORDER BY deslogin");
 
-print_r($usuarios)
+$headers = array();
+
+foreach ($usuarios[0] as $key => $value){
+    array_push($headers , ucfirst($key));
+}
+
+$file = fopen("usuarios.csv" , "w+");
+
+fwrite($file , implode("," , $headers) . "\r\n");
+
+foreach ($usuarios as $row){
+    $data = array();
+
+    foreach ($row as $key =>$value){
+        array_push($data, $value);
+    } // end foreach de coluna
+
+    fwrite($file, implode("," , $data) . "\r\n");
+
+} //end foreach de linha
+fclose($file);
 
 ?>
